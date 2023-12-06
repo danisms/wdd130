cc10c37c4c2fd59c66e07c12013af8d59bb32168
danisms <danielcopute@gmail.com>
2023-12-06 11:38:44 +0100

    completed my contact me page

--- my_porfolio/js/about.js
+++ my_porfolio/js/about.js
@@ -10,8 +10,8 @@ function changePhotos() {
 	//image list
 	images[0] = 'images/about_me/daniel_opute.jpg'
     images[1] = 'images/about_me/daniel_opute.jpg'
-    images[2] = 'images/about_me/daniel_opute.jpg'
-    images[3] = 'images/about_me/danielcopute.png'
+    images[2] = 'images/about_me/danielcopute.png'
+    // images[3] = 'images/about_me/danielcopute.png'
     // images[4] = 'images/about_me/danielcopute.png'
     // images[2] = 'images/home/slide-img/megasoftouch-h.png'
     // images[3] = 'images/home/slide-img/megasoftouch.png'
@@ -19,18 +19,21 @@ function changePhotos() {
     lastImg = 'images/about_me/danielcopute.png'
 
     let imgOne = document.getElementById('my_photos')
+    let imgTwo = document.getElementById('my_photos_2')
 
 	function changeImg(){
         if(k < images.length){
             imgOne.src = images[k];
+            imgTwo.src = images[k];
             k++;
-            time = 12700
+            time = 13500
             setTimeout("changePhotos()", time);
         }
         else {
             time = 20050
             setTimeout("changePhotos()", time);
             imgOne.src = lastImg;
+            imgTwo.src = lastImg;
             k = 0;
         }
 	}
